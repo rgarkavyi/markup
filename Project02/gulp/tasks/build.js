afc5cba19ff75f503d52eb044892019623c6174f
rgarkavyi <53447149+rgarkavyi@users.noreply.github.com>
2019-12-17 21:42:35 +0200

    Add files via upload

---
+++ Project02/gulp/tasks/build.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    $.gulp.task(
+        "build",
+        $.gulp.series(
+            "clean",
+            "copy",
+            "sass",
+            "scripts",
+            "images",
+            "webp",
+            "svg"
+        )
+    );
+};
